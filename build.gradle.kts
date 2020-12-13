109f766dfbeb8bec837449b124fd19b6d0cc9e5c
Victor <10775422+vnea@users.noreply.github.com>
2020-12-13 21:34:28 +0100

    tech: Add essential files

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.4.10"
+}
+
+group = "com.vnea"
+version = "0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
