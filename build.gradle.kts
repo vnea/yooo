962594c3216cc2bd7471c309570abc41c7523689
Victor Nea <vnea@octo.com>
2020-12-14 13:36:02 +0100

    tech: Set JVM target to 11

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,12 @@ plugins {
     kotlin("jvm") version "1.4.10"
 }
 
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
 group = "com.vnea"
 version = "0.1"
 
