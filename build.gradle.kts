5d884fe2756f4e0a6c7cc1047065a981a5812624
Victor Nea <vnea@octo.com>
2020-12-14 14:21:13 +0100

    chore: Add Spotless to auto-format Kotlin and Gradle files

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.4.10"
+    id("com.diffplug.spotless") version "5.8.2"
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
@@ -19,4 +20,14 @@ dependencies {
     implementation(kotlin("stdlib"))
     implementation("org.springframework.boot:spring-boot-starter-webflux:2.4.1")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.0")
+}
+
+configure<com.diffplug.gradle.spotless.SpotlessExtension> {
+    kotlin {
+        ktfmt("0.18").dropboxStyle()
+    }
+
+    kotlinGradle {
+        ktlint("0.40.0")
+    }
 }
--- src/main/kotlin/com/vnea/yooo/Application.kt
+++ src/main/kotlin/com/vnea/yooo/Application.kt
@@ -4,11 +4,8 @@ import org.springframework.boot.Banner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
-open class Application
+@SpringBootApplication open class Application
 
 fun main(args: Array<String>) {
-    runApplication<Application>(*args) {
-        setBannerMode(Banner.Mode.OFF)
-    }
+    runApplication<Application>(*args) { setBannerMode(Banner.Mode.OFF) }
 }
